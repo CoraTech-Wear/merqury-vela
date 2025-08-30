ab8e32cf78387df45b31f0866d7486107dc1f88b
lesetong <oscarlbw@qq.com>
2025-08-30 21:51:14 +0800

    feat(图片缓存): 添加图片缓存时间参数并优化图片组件
    refactor(imgCache): 修改getImage函数支持缓存时间参数
    feat(BetterOnlineImage): 使用新的缓存机制并优化样式处理
    feat(index): 添加下拉刷新功能并替换图片组件

--- src/utils/imgCache.js
+++ src/utils/imgCache.js
@@ -7,27 +7,36 @@ const cacheIng = new Map()
 /**
  * 获取图片的缓存路径,如果没有缓存则下载并缓存
  * @param {String} url 图片的url
+ * @param {number} time 缓存时间
  * @returns {Promise<String>} 图片的缓存路径
  * @author lesetong
  */
-export async function getImage(url) {
+export async function getImage(url,time) {
     /* const { baseUrl, fop } = buffImg.splitUrlAndFop(url) */
     const cachePath = getCachePath(url)
     try {
-        await runAsyncFunc(file.access, { uri: cachePath })
-        return cachePath
+        const {lastModifiedTime}=await runAsyncFunc(file.get, { uri: cachePath })
+        if(!time||lastModifiedTime+time>Date.now()){
+            return cachePath
+        }
     } catch (e) {
         if (e.code !== 301) throw e
     }
-    if (cacheIng.has(cachePath)) { return await cacheIng.get(cachePath) }
-    let res
-    cacheIng.set(cachePath,new Promise((resolve, reject) => { res = resolve }))
-    const { token } = await runAsyncFunc(request.download, { url, filename: cachePath })
-    const onDownloadComplete = runAsyncFunc(request.onDownloadComplete, { token })
-    const { uri } = await onDownloadComplete
-    res(uri)
-    cacheIng.delete(cachePath)
-    return uri
+    try {
+        if (cacheIng.has(cachePath)) { return await cacheIng.get(cachePath) }
+        let res
+        cacheIng.set(cachePath,new Promise((resolve, reject) => { res = resolve }))
+        const { token } = await runAsyncFunc(request.download, { url, filename: cachePath })
+        const onDownloadComplete = runAsyncFunc(request.onDownloadComplete, { token })
+        const { uri } = await onDownloadComplete
+        res(uri)
+        cacheIng.delete(cachePath)
+        return uri
+    } catch (error) {
+        global.logger.error("[imgCache] getImage error",error)
+        res(cachePath)
+        return cachePath
+    }
 }
 function getCachePath(url) {
     const name = crypto.hashDigest({ data: url, algo: "MD5" })
