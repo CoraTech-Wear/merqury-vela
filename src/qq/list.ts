0cb38e7f6b9a144436ed7164f52a30fae73b0b76
2secRain <156428383+2secRain@users.noreply.github.com>
2025-11-04 01:13:11 +0800

    修复部分设置项无法打开的问题; 增加了对服务器无法连接时的错误提示

--- src/qq/list.ts
+++ src/qq/list.ts
@@ -1,7 +1,13 @@
 import { sendNapCatPostRequest } from "./request";
 
 export async function getRecentContact(count: Number): Promise<any> {
-    return (await sendNapCatPostRequest("/get_recent_contact", {
-        count: count
-    }));
+    try{
+        return (await sendNapCatPostRequest("/get_recent_contact", {
+            count: count
+        }));
+    }
+    catch(err){
+        throw err;
+    }
+    
 }
